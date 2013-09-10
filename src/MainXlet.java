08d8631d3d8fa3fbbc239ab8535411d42bd7f817
taylsonmartinez <taylson.martinez@gmail.com>
2013-09-09 23:31:41 -0300

    Project configured for use with examples

---
+++ src/MainXlet.java
@@ -0,0 +1,23 @@
+
+import javax.microedition.xlet.Xlet;
+import javax.microedition.xlet.XletContext;
+import javax.microedition.xlet.XletStateChangeException;
+
+import com.sun.dtv.lwuit.*;
+
+
+public class MainXlet implements Xlet {
+
+	public void destroyXlet(boolean arg0) throws XletStateChangeException {
+	}
+
+	public void initXlet(XletContext arg0) throws XletStateChangeException {
+	}
+
+	public void pauseXlet() {
+	}
+
+	public void startXlet() throws XletStateChangeException {
+	}
+
+}
